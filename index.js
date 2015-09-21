e529a998bcc85b706eb4b608705683f6a34dbcea
eabatedaga <enrico.abate-daga@bea-services.de>
2015-09-21 16:43:44 +0200

    initial version

---
+++ index.js
@@ -0,0 +1,29 @@
+var fs = require ('fs');
+  lazy = require('lazy');
+
+
+  var myLookup = new DNTransform();
+  myLookup.init();
+
+new lazy (fs.createReadStream('VTK.COSMA.COM_2015-08-18 13%3A13%3A56Z.txt'))
+  .lines
+  .forEach(function(line){
+    console.log("Line:" + myLookup.getPrettyName(line);
+  }
+);
+
+
+function DNTransform() {
+
+  this.init = function() {
+
+    //load rules in text files
+
+  }
+
+  this,getPrettyName = function (thisLine){
+    //lookup the name using things cached in init
+    return "whatever";
+  }
+
+}
